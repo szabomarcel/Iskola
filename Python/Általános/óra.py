69cc0e89e797830d88bdfb2ce77b6b9785f9b49d
Snewkovits <adamhunxd@gmail.com>
2022-11-21 17:01:23 +0100

    main

--- Python/Általános/óra.py
+++ Python/Általános/óra.py
@@ -1,8 +1,9 @@
+import time
 import turtle
-from time import *
 
 screen = turtle.Screen()
 t = turtle.Turtle()
+t.color("black")
 t.pensize(3)
 t.penup()
 t.speed(0)
@@ -14,4 +15,17 @@ for i in range(12):
    t.penup()
    t.back(101)
    t.right(30)
-   
+
+t.left(90)
+t.pendown()
+t.hideturtle()
+
+while True:
+   t.color("black")
+   t.forward(90)
+   t.back(90)
+   time.sleep(1)
+   t.color("white")
+   t.forward(90)
+   t.back(90)
+   t.right(6)
