474648d7fbde9c996442f6be79a9251c4e62eaa9
Snewkovits <adamhunxd@gmail.com>
2022-11-29 10:43:18 +0100

    .

--- Python/5.14/10.py
+++ Python/5.14/10.py
@@ -4,8 +4,8 @@ b = 0
 isDone = True
 while isDone:
    try:
-      a = int(input("Írja be az első befogót hosszát (cm): "))
-      b = int(input("Írja be a második befogót hosszát (cm): "))
+      a = int(input("Írja be az első befogó hosszát (cm): "))
+      b = int(input("Írja be a második befogó hosszát (cm): "))
       isDone = False
    except:
       print("Rossz adatbevitel!")
