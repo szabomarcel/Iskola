bbe5eae963538c62875a960d5bff7f7cabb6355a
Snewkovits <adamhunxd@gmail.com>
2022-12-05 15:05:23 +0100

    .

--- Python/Általános/ag.py
+++ Python/Általános/ag.py
@@ -33,3 +33,26 @@ else:
 """
 
 #3
+import random
+
+class Szere:
+   def __init__(self, nev, fog, nem, szam):
+      self.nev = nev
+      self.fog = fog
+      self.nem = nem
+      self.szam = szam
+   
+   def FvN(nem):
+      if nem.lower() == "f": return "Férfi"
+      elif nem.lower() == "n": return "Nő"
+
+t = []
+for x in range(3):
+   a = input("Add meg a nevet: ")
+   b = input("Add meg a nemet (f/n): ")
+   c = input("Add meg a foglalkozást: ")
+   d = random.randint(1,50)
+   t.append(Szere(a,b,c,d))
+
+for x in t:
+   print(x.nev)
