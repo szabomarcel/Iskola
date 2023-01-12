714c7a5a311da89bb21685af4e190e23e66f802b
Snewkovits <adamhunxd@gmail.com>
2023-01-12 12:23:58 +0100

    .

--- Python/13.11/4/program.py
+++ Python/13.11/4/program.py
@@ -1,26 +1,28 @@
 table = []
 lines = []
 file = open("Python\\13.11\\3\\output.txt", "r")
-
 for line in file:
    for l in line.split():
-      lines.append(line)
+      lines.append(l)
    table.append(lines)
-   lines = [] 
+   lines = []
 file.close()
 
+doneTable = []
+doneLines = []
 index = 0
-col_index = 0
 for i in range(len(table)):
    for j in range(len(table[i])):
       if index < 5 and j % 2 == 0:
-         table[i].remove(table[i][index * 2])
+         doneLines.append(table[i][(index + 1) * 2])
          index += 1
+      else: doneLines.append(table[i][j])
+      doneTable.append(doneLines)
    index = 0
 
 file = open("Python\\13.11\\4\\output.txt", "w")
-for t in table:
-   for line in t:
-      file.write(f"{line} ")
+for lines in doneTable:
+   for words in lines:
+      file.write(f" {words}")
    file.write("\n")
 file.close()
